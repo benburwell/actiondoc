e579f27cae19d8f3b16ee17819be71d4a3701592
Ben Burwell <bburwell1@gmail.com>
2014-07-27 17:02:36 -0400

    Switch to node.js

---
+++ app.js
@@ -0,0 +1,33 @@
+var paperboy = require('paperboy'),
+    http = require('http'),
+    path = require('path');
+
+var webroot = path.join(__dirname, 'public'),
+    port = 8080;
+
+http.createServer(function(req, res) {
+  var ip = req.connection.remoteAddress;
+  paperboy
+    .deliver(webroot, req, res)
+    .addHeader('X-Powered-By', '')
+    .before(function() {
+      console.log('Request received for ' + req.url);
+    })
+    .after(function(statusCode) {
+      console.log(statusCode + ' - ' + req.url + ' ' + ip);
+    })
+    .error(function(statusCode, msg) {
+      console.log([statusCode, msg, req.url, ip].join(' '));
+      res.writeHead(statusCode, { 'Content-Type': 'text/plain' });
+      res.end('Error [' + statusCode + ']');
+    })
+    .otherwise(function(err) {
+      console.log([404, err, req.url, ip].join(' '));
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('Error 404: File not found');
+    });
+}).listen(port);
+ 
+setInterval(function () {
+    http.get('http://actiondoc.benburwell.com/');
+}, 45 * 60 * 1000);
